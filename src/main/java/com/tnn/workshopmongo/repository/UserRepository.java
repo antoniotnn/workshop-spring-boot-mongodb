0df4d759d9090d4d4a0c701339c46ea88bb90b04
Antonio Pires <tonan@globo.com>
2021-01-22 07:20:19 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/tnn/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.tnn.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tnn.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
+	
+}
---
+++ src/main/java/com/tnn/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.tnn.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tnn.workshopmongo.domain.User;
+import com.tnn.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
