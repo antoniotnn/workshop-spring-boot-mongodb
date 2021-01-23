96c9f3106d94c591d89e972e845be950ee25fbb6
Antonio Pires <tonan@globo.com>
2021-01-23 10:01:47 -0300

    Obtendo post por id

---
+++ src/main/java/com/tnn/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.tnn.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tnn.workshopmongo.domain.Post;
+import com.tnn.workshopmongo.repository.PostRepository;
+import com.tnn.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
